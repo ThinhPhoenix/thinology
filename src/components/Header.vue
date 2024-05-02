e36bb3cfd101a026dc3b0230224255bc90974e1b
ThinhPhoenix <laithinh1310@gmail.com>
2024-05-02 20:27:43 +0700

    dsa

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
         <router-link to="/">Discover</router-link>
         <router-link to="/about">About</router-link>
         <router-link to="/help">Help</router-link>
-        <router-link to="/cookyourfone" target="_blank">FRPBypass</router-link>
+        <router-link to="/cookyourfone">FRPBypass</router-link>
     </nav>
 </template>
 
