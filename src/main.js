416b20b5e41ee8bcebef4beb75ce75fb29b67599
ThinhPhoenix <laithinh1310@gmail.com>
2024-05-02 20:30:26 +0700

    update

--- src/main.js
+++ src/main.js
@@ -6,13 +6,3 @@ import router from './router'
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
-
-const currentPath = window.location.pathname;
-const pathToHide = '/cookyourfone';
-
-if (currentPath === pathToHide) {
-    const elements = document.getElementsByClassName('hid-frp');
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].style.display = 'none';
-    }
-}
