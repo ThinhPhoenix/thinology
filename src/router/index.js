2d69e62f3864ac146bc807a7eec7f5ae11665c69
ThinhPhoenix <laithinh1310@gmail.com>
2024-05-03 17:27:55 +0700

    -rm error page

--- src/router/index.js
+++ src/router/index.js
@@ -4,14 +4,12 @@ import Discover from "../screens/Discover.vue";
 import About from "../screens/About.vue";
 import Help from "../screens/Help.vue";
 import FRPBypass from "../screens/FRPBypass.vue";
-import Error from "../screens/Error.vue";
 
 const routes = [
     {path: '/', component: Discover},
     {path: '/about', component: About},
     {path: '/help', component: Help},
     {path: '/cookyourfone', component: FRPBypass},
-    {path: '/:err(.*)*', component: Error},
 ]
 
 const router = createRouter({
